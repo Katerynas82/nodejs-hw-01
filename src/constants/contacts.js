ac458b0f17a664af3e19aebe5742517f64e5865d
Kateryna Sukovaia <122564463+Katerynas82@users.noreply.github.com>
2024-11-13 14:41:53 +0000

    ok

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,4 +1,4 @@
 
 import path from 'node:path';
-export const PATH_DB = path.join(__dirname, '../db/db.json');
+export const PATH_DB = path.resolve('src/db/db.json'); 
 
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import { countContacts } from './scripts/countContacts.js';
 
 import { addOneContact } from './scripts/addOneContact.js';
 import { getAllContacts } from './scripts/getAllContacts.js';
-// import { removeAllContacts } from './src/scripts/removeAllContacts.js';
-// import { removeLastContact } from './src/scripts/removeLastContact.js';
-// import { generateContacts } from './src/scripts/generateContacts.js';
+// import { removeAllContacts } from './scripts/removeAllContacts.js';
+// import { removeLastContact } from './scripts/removeLastContact.js';
+// import { generateContacts } from './scripts/generateContacts.js';
 
 async () => {
   console.log('Кількість контактів:', await countContacts());
